17a85ff53b7c9b5ec6ab656e0e0e080277cc905d
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-07-18 13:48:17 +0530

    added Embassy Migration

--- MigrantWorkers/Data/ApplicationDbContext.cs
+++ MigrantWorkers/Data/ApplicationDbContext.cs
@@ -13,6 +13,7 @@ namespace MigrantWorkers.Data
 
         public DbSet<User> Users { get; set; }
         public DbSet<SLFB_User> SLFBUsers { get; set; }
-        public DbSet<MigrantWorkers.Models.SLFB_User_Input>? SLFB_User_Input { get; set; }
+        //public DbSet<MigrantWorkers.Models.SLFB_User_Input>? SLFB_User_Input { get; set; }
+        public DbSet<Embassy> Embassies { get; set; }
     }
 }
