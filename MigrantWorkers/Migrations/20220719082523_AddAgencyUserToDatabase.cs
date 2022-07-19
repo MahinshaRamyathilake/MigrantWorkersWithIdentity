0a7ed97a9d62ebc2e9359100061b7a5623a8da6d
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-07-19 13:57:16 +0530

    added agency user migration

---
+++ MigrantWorkers/Migrations/20220719082523_AddAgencyUserToDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrantWorkers.Migrations
+{
+    public partial class AddAgencyUserToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AgencyUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    AgencyID = table.Column<int>(type: "int", nullable: false),
+                    Fname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Lname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgencyUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AgencyUsers_Agencies_AgencyID",
+                        column: x => x.AgencyID,
+                        principalTable: "Agencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AgencyUsers_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgencyUsers_AgencyID",
+                table: "AgencyUsers",
+                column: "AgencyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgencyUsers_UserID",
+                table: "AgencyUsers",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgencyUsers");
+        }
+    }
+}
