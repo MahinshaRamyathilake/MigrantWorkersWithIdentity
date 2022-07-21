b64161e7db76f59a8a613646423969985ee3c7e6
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-07-21 13:55:58 +0530

    added embassy user migration

--- MigrantWorkers/Data/ApplicationDbContext.cs
+++ MigrantWorkers/Data/ApplicationDbContext.cs
@@ -21,5 +21,7 @@ namespace MigrantWorkers.Data
         public DbSet<Agency_User> AgencyUsers { get; set; }
 
         //public DbSet<MigrantWorkers.Models.Agency_User_Input>? Agency_User_Input { get; set; }
+
+        public DbSet<Embassy_User> EmbassyUsers { get; set; }
     }
 }
---
+++ MigrantWorkers/Migrations/20220721082324_AddEmbassyUserToDatabase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MigrantWorkers.Migrations
+{
+    public partial class AddEmbassyUserToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EmbassyUsers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    UserID = table.Column<int>(type: "int", nullable: false),
+                    EmbassyID = table.Column<int>(type: "int", nullable: false),
+                    Fname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Lname = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Address = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Email = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmbassyUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmbassyUsers_Embassies_EmbassyID",
+                        column: x => x.EmbassyID,
+                        principalTable: "Embassies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmbassyUsers_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmbassyUsers_EmbassyID",
+                table: "EmbassyUsers",
+                column: "EmbassyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_EmbassyUsers_UserID",
+                table: "EmbassyUsers",
+                column: "UserID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmbassyUsers");
+        }
+    }
+}
