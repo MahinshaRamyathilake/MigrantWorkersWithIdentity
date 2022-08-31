f85ba376004aa219f6b2352bc1b77ec5706a3ef0
MahinshaRamyathilake <mahiramyathilake@gmail.com>
2022-08-31 09:38:12 +0530

    added Controller

---
+++ MigrantWorkers/Controllers/FamilyDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MigrantWorkers.Controllers
+{
+    public class FamilyDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MigrantWorkers/Controllers/QueryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MigrantWorkers.Controllers
+{
+    public class QueryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
